397463f223c9bb29eb6b5beeddfa10c325d1f2ba
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2023-11-03 02:16:55 +0530

    data ingestion

---
+++ src/cnnClassifier/components/data_ingestion.py
@@ -0,0 +1,43 @@
+import os
+import urllib.request as request
+from zipfile import ZipFile
+from tqdm import tqdm
+from pathlib import Path
+from cnnClassifier.entity import DataIngestionConfig
+from cnnClassifier import logger
+from cnnClassifier.utils import get_size
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+    
+    def download_file(self):
+        logger.info("Trying to download file...")
+        if not os.path.exists(self.config.local_data_file):
+            logger.info("Download started...")
+            filename , headers = request.urlretrieve(
+                url = self.config.source_URL,
+                filename = self.config.local_data_file
+            )
+            logger.info(f"{filename} downloaded! with following info: \n{headers}")
+        
+
+    def _get_updated_list_of_files(self, list_of_files):
+        return [f for f in list_of_files if f.endswith(".jpg") and ('Cat' in f or 'Dog' in f)]
+
+    def _preprocess(self, zf: ZipFile, f: str, working_dir: str):
+        target_filepath = os.path.join(working_dir, f)
+        if not os.path.exists(target_filepath):
+            zf.extract(member=f, path=working_dir)
+            
+        if os.path.getsize(target_filepath) == 0:
+            os.remove(target_filepath)
+
+    def unzip_and_clean(self):
+        with ZipFile(file=self.config.local_data_file, mode='r') as zf:
+            list_of_files = zf.namelist()
+            updated_list_of_files = self._get_updated_list_of_files(list_of_files)
+            for f in updated_list_of_files:
+                self._preprocess(zf, f, self.config.unzip_dir)
+
+
